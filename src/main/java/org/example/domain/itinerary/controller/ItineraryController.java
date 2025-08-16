c2f8647e4d7dc6ac1e3d1768e13ac23da02dfe05
JeongHyeon Jo <cisxo@naver.com>
2025-08-16 21:32:38 +0900

    feat(Itinerary): feature input process && getMaxAutoIncrement

--- src/main/java/org/example/domain/itinerary/controller/ItineraryController.java
+++ src/main/java/org/example/domain/itinerary/controller/ItineraryController.java
@@ -1,19 +1,25 @@
 package org.example.domain.itinerary.controller;
 
 import org.example.domain.itinerary.entity.Itinerary;
+import org.example.domain.itinerary.service.ItineraryService;
 import org.example.domain.trip.entity.Trip;
 import org.example.domain.trip.service.TripService;
+import org.example.view.inputView.InputView;
+import org.example.view.outputView.OutputView;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 public class ItineraryController {
-    public static TripService tripService = new TripService();
-    public static List<Itinerary> itineraries = new ArrayList<>();
+    public TripService tripService = new TripService();
+    public ItineraryService itineraryService = new ItineraryService();
+    public List<Itinerary> itineraries = new ArrayList<>();
+    public OutputView outputView = new OutputView();
+    public InputView inputView = new InputView();
 
     public List<Itinerary> getIterineriesFromTrips(int targetTripId) {
-        List<Trip> allTrips = tripService.getTrip();
+        List<Trip> allTrips = tripService.initialMappingJsonFile();
         Optional<Trip> foundTrip = allTrips.stream()
                 .filter(trip -> trip.getTrip_id() == targetTripId)
                 .findFirst();
@@ -35,5 +41,40 @@ public class ItineraryController {
     }
 
     public void inputItinearyData() {
+        getAllTripViewToItineraryInput();
+        int selectTripInputId = inputView.inputData();
+
+        int chooseRetry;
+        do {
+            outputView.startItineraryInputStartMessage();
+
+            outputView.tripRequestItineraryOrigin();
+            String tripOriginName = inputView.inputDataStr();
+
+            outputView.tripRequestItineraryDestination();
+            String tripDestination = inputView.inputDataStr();
+
+            outputView.tripRequestItineraryOriginTime();
+            String tripOriginTime = inputView.inputDataStrTime();
+
+            outputView.tripRequestItineraryDestinationTime();
+            String tripDestinationTime = inputView.inputDataStrTime();
+
+            outputView.tripRequestItineraryCheckInTime();
+            String tripCheckInTime = inputView.inputDataStrTime();
+
+            outputView.tripRequestItineraryCheckOutTime();
+            String tripCheckOutTime = inputView.inputDataStrTime();
+
+            itineraryService.saveItineraryInfo(selectTripInputId, tripOriginName, tripDestination, tripOriginTime, tripDestinationTime, tripCheckInTime, tripCheckOutTime);
+            outputView.sucessSaveItineraryInfoMessage();
+            chooseRetry = inputView.inputData();
+
+        } while (chooseRetry == 1);
+    }
+
+    private void getAllTripViewToItineraryInput() {
+        List<Trip> getTrips = tripService.findAllTrips();
+        outputView.viewerGetTrips(getTrips);
     }
 }
